e6d37601038432cbdb08aaa7db245a219edf7fd5
Ariel <4540263497@estudante.sed.sc.gov.br>
2023-02-14 20:40:26 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -17,3 +17,10 @@ console.log('fora1 = ',numero1)
 const numero2 = 10
 numero2 = 11
 
+let numero = 10
+let numero2 = 20
+let numero3 = 10 + 20
+let numero4 = 50
+
+let soma = numero + numero2
+
---
+++ src/operadores.js
@@ -0,0 +1,17 @@
+let numero = 10;
+let numero2 = 20;
+let numero3 = 10 + 20;
+let numero4 = 50;
+
+let soma = numero + numero2;
+let sub = numero - numero2;
+let div = numero / numero2;
+let mult = numero * numero2;
+let numero5 = numero4;
+
+console.log(soma);
+console.log(sub);
+console.log(div);
+console.log(mult);
+console.log(numero3);
+console.log(numero5);
