5d3f926444a180816780c79495acd9adb8170e4c
ArielEichstadt <125396879+ArielEichstadt@users.noreply.github.com>
2023-02-14 17:35:18 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+let numero = 1
+{
+  let numero = 2
+  console.log ('dentro =',numero)
+} 
+console.log('fora=',numero)
+
+var numero1 = 1
+
+{
+  var numero1 = 2
+  console.log('dentro1=',numero1)
+}
+
+console.log('fora1 = ',numero1)
+
+const numero2 = 10
+numero2 = 11
+
